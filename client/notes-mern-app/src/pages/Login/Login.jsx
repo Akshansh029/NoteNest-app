6576bd7dde16f9188abc66766e219b13b3f81fa6
akshanshsingh00 <akshanshsingh00@gmail.com>
2024-06-07 14:22:22 +0530

    Login page created

--- client/notes-mern-app/src/pages/Login/Login.jsx
+++ client/notes-mern-app/src/pages/Login/Login.jsx
@@ -1,9 +1,31 @@
+import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 
 const Login = () => {
   return (
     <>
       <Navbar />
+
+      <div className="flex justify-center items-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form onSubmit={() => {}} className="">
+            <h4 className="text-2xl mb-7">Login</h4>
+
+            <input type="text" placeholder="Email" className="input-box" />
+
+            <button type="submit" className="btn-primary">
+              Login
+            </button>
+
+            <p className="text-sm text-center mt-4">
+              Don't have an account{" "}
+              <Link to="/signup" className="text-blue-600 underline">
+                Create an account
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
--- client/notes-mern-app/tailwind.config.js
+++ client/notes-mern-app/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2BB5FF",
+        seconday: "EF863E",
+      },
+    },
   },
   plugins: [],
 };

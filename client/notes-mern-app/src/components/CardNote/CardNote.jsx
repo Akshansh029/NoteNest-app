d76dcc8a1e8810c1437c5bd126febc9b4c48186a
akshanshsingh00 <akshanshsingh00@gmail.com>
2024-06-09 11:48:53 +0530

    Home page ui completed

--- client/notes-mern-app/src/components/CardNote/CardNote.jsx
+++ client/notes-mern-app/src/components/CardNote/CardNote.jsx
@@ -12,21 +12,28 @@ const CardNote = ({
   onPinNote,
 }) => {
   return (
-    <div>
-      <div className="">
-        <div className="">
+    <div className="border-2 rounded p-4 hover:shadow-xl transition-all ease-in-out">
+      <div className="flex items-center justify-between">
+        <div>
           <h5 className="text-base font-medium">{title}</h5>
           <span className="text-xs text-slate-500">{date}</span>
         </div>
-        <MdOutlinePushPin className="" onClick={onPinNote} />
+
+        <MdOutlinePushPin
+          className={`icon-btn ${isPinned ? "text-primary" : "text-slate-400"}`}
+          onClick={onPinNote}
+        />
       </div>
-      <p className="text-sm text-slate-700">{content?.slice(0, 60)}...</p>
-      <div>
+      <p className="text-sm mt-2 text-slate-600">{content?.slice(0, 60)}...</p>
+      <div className="flex items-center justify-between mt-2">
         <div className="text-xs text-slate-400">{tags}</div>
 
         <div className="flex items-center gap-2">
           <MdEdit className="icon-btn hover:text-green-600" />
-          <MdDelete className="icon-btn hover:text-red-600" />
+          <MdDelete
+            className="icon-btn hover:text-red-600"
+            onClick={deleteNote}
+          />
         </div>
       </div>
     </div>
--- client/notes-mern-app/src/pages/Home/Home.jsx
+++ client/notes-mern-app/src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../../components/Navbar/Navbar";
 import CardNote from "../../components/CardNote/CardNote";
+import { FaPlus } from "react-icons/fa6";
 
 const Home = () => {
   return (
@@ -7,14 +8,60 @@ const Home = () => {
       <Navbar />
 
       <div className="container mx-auto">
-        <CardNote
-          title="Meeting with Rimjhim at 5pm"
-          date="16 June 2024"
-          tags="#meeting"
-          content="Will meet Rimjhim in bilaspurat 5 pm and will hangout for sometime"
-          isPinned={true}
-        />
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          <CardNote
+            title="Meeting with Rimjhim at 5pm"
+            date="16 June 2024"
+            tags="#meeting"
+            content="Will meet Rimjhim in bilaspurat 5 pm and will hangout for sometime"
+            isPinned={true}
+            deleteNote={() => {}}
+            onPinNote={() => {}}
+          />
+          <CardNote
+            title="Meeting with Rimjhim at 5pm"
+            date="16 June 2024"
+            tags="#meeting"
+            content="Will meet Rimjhim in bilaspurat 5 pm and will hangout for sometime"
+            isPinned={true}
+            deleteNote={() => {}}
+            onPinNote={() => {}}
+          />
+          <CardNote
+            title="Meeting with Rimjhim at 5pm"
+            date="16 June 2024"
+            tags="#meeting"
+            content="Will meet Rimjhim in bilaspurat 5 pm and will hangout for sometime"
+            isPinned={true}
+            deleteNote={() => {}}
+            onPinNote={() => {}}
+          />
+          <CardNote
+            title="Meeting with Rimjhim at 5pm"
+            date="16 June 2024"
+            tags="#meeting"
+            content="Will meet Rimjhim in bilaspurat 5 pm and will hangout for sometime"
+            isPinned={true}
+            deleteNote={() => {}}
+            onPinNote={() => {}}
+          />
+          <CardNote
+            title="Meeting with Rimjhim at 5pm"
+            date="16 June 2024"
+            tags="#meeting"
+            content="Will meet Rimjhim in bilaspurat 5 pm and will hangout for sometime"
+            isPinned={true}
+            deleteNote={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
       </div>
+      <button className="bg-primary p-4 absolute right-10 bottom-10 rounded-lg hover:bg-blue-500 hover:drop-shadow-md flex items-center gap-2">
+        <FaPlus className="text-white text-[30px]" />
+        <span className="hidden text-white font-medium group-hover:flex">
+          Create note
+        </span>
+      </button>
     </>
   );
 };

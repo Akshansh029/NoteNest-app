88cdc3f131f83b9d87af2ea64bf4ae0c19c1ac5b
akshanshsingh00 <akshanshsingh00@gmail.com>
2024-06-09 22:35:50 +0530

    Login API completed

--- server/app.js
+++ server/app.js
@@ -8,11 +8,12 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 5000;
+const User = require("./models/user.model");
 
-// const jwt = require("jsonwebtoken");
-// import { authenticateToken } from "./utilities";
+const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./utilities");
 
-app.use(express.json);
+app.use(express.json());
 
 app.use(
   cors({
@@ -24,8 +25,90 @@ app.get("/", (req, res) => {
   res.json({ data: "Hello world" });
 });
 
-//Create account
+// Create account API
+app.post("/create-account", async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  if (!fullName) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Fullname is required" });
+  }
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email is required" });
+  }
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required" });
+  }
+
+  const isUser = await User.findOne({ email: email });
+
+  if (isUser) {
+    console.log("User check:", isUser);
+    return res.json({ error: true, message: "User already exists" });
+  }
+
+  const user = new User({
+    fullName,
+    email,
+    password,
+  });
+
+  await user.save();
+
+  const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_KEY, {
+    expiresIn: "3600m",
+  });
+
+  return res.json({
+    error: false,
+    user,
+    accessToken,
+    message: "Registration Successful",
+  });
+});
+
+//Login API
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.json({ error: true, message: "Email is required" });
+  }
+  if (!password) {
+    return res.json({ error: true, message: "Password is required" });
+  }
+
+  const userInfo = await User.findOne({ email: email });
+
+  if (!userInfo) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  if (userInfo.email == email && userInfo.password == password) {
+    const user = { user: userInfo };
+    const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_KEY, {
+      expiresIn: "3600m",
+    });
+
+    return res.status(200).json({
+      error: false,
+      email,
+      accessToken,
+      message: "Login successful",
+    });
+  } else {
+    return res.status(400).json({
+      error: true,
+      message: "Invalid credentials",
+    });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
+module.exports = app;
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -8,4 +8,4 @@ const userSchema = new Schema({
   createdOn: { type: Date, defualt: new Date().getTime() },
 });
 
-model.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);

3e391345fcd8822b6f799ded0528ef742a070c18
akshanshsingh00 <akshanshsingh00@gmail.com>
2024-06-22 01:28:48 +0530

    Adjusted profileInfo

--- client/src/components/ProfileInfo/ProfileInfo.jsx
+++ client/src/components/ProfileInfo/ProfileInfo.jsx
@@ -29,7 +29,7 @@ const ProfileInfo = ({ onLogout, userInfo, isDarkMode }) => {
       </div>
       <div className="relative group">
         <button
-          className={`text-sm font-medium py-[5px] px-3 rounded-3xl transition-all ease-in group`}
+          className={`text-sm font-medium py-[5px] px-1 rounded-3xl transition-all ease-in group`}
           onClick={onLogout}
         >
           <MdLogout
